ec327efe126655a3500a00fe43d0dc754dd30686
Mahmoud123-Makhaly <mahmoud.abdelfatah@5d-agency.com>
2025-11-24 18:19:58 +0200

    -

---
+++ app/(productList)/categories/page.tsx
@@ -0,0 +1,19 @@
+const Categories = async () => {
+  const res = await fetch("http://localhost:4000/category");
+  const data = await res.json();
+  console.log(data);
+  return (
+    <>
+      <h1 className="pt-24"> Categories </h1>
+      {data.map((product: { id: number; title: string; price: number }) => (
+        <div
+          key={product.id}
+          className="bg-red-500 mb-4 w-[500px] p-4 rounded text-white"
+        >
+          <h1>{product.title}</h1>
+        </div>
+      ))}
+    </>
+  );
+};
+export default Categories;
--- app/(productList)/products/page.tsx
+++ app/(productList)/products/page.tsx
@@ -1,10 +1,32 @@
-const page = () => {
+"use client";
+
+import { useEffect, useState } from "react";
+
+const Products = () => {
+  const [products, setProducts] = useState<
+    { id: number; title: string; price: number }[]
+  >([]);
+  useEffect(() => {
+    const fetchAllProducts = async () => {
+      const res = await fetch("http://localhost:4000/products");
+      const data = await res.json();
+      setProducts(data);
+    };
+    fetchAllProducts();
+  }, []);
+
   return (
-    <main>
-      <h1 className="bg-red-500 h-screen">products page</h1>
-      <h1 className=" h-screen">products page</h1>
-      <h1 className="bg-red-500 h-screen">products page</h1>
+    <main className="pt-32">
+      {products.map((product) => (
+        <div
+          key={product.id}
+          className="bg-red-500 mb-4 w-[500px] p-4 rounded text-white"
+        >
+          <h1>{product.title}</h1>
+          <p>{product.price}</p>
+        </div>
+      ))}
     </main>
   );
 };
-export default page;
+export default Products;
